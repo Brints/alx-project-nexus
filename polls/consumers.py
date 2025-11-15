3ea2e73915f6c40c65aad828b5068b4d0f724488
aniebietafia <aniebietafia87@gmail.com>
2025-11-15 22:16:28 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    feat: completed authentication service.
    
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>
    
    feat: completed authentication service.
    
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>
    
    # This is the commit message #2:
    
    feat: completed authentication service.
    
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

--- polls/consumers.py
+++ polls/consumers.py
@@ -1,4 +1,5 @@
 import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
@@ -9,36 +10,33 @@ class PollConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         # Get poll_id from the URL route
-        self.poll_id = self.scope['url_route']['kwargs']['poll_id']
-        self.room_group_name = f'poll_{self.poll_id}'
+        self.poll_id = self.scope["url_route"]["kwargs"]["poll_id"]
+        self.room_group_name = f"poll_{self.poll_id}"
 
         # Join the room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         # Accept the WebSocket connection
         await self.accept()
 
         # Send a simple "connected" message
-        await self.send(text_data=json.dumps({
-            'type': 'connection_established',
-            'message': f'Connected to poll {self.poll_id}'
-        }))
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "type": "connection_established",
+                    "message": f"Connected to poll {self.poll_id}",
+                }
+            )
+        )
 
     async def disconnect(self, close_code):
         # Leave the room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Handler for messages broadcasted to the group
     async def poll_update(self, event):
-        results = event['results']
+        results = event["results"]
         # Send the message down to the WebSocket client
-        await self.send(text_data=json.dumps({
-            'type': 'poll_update',
-            'results': results
-        }))
+        await self.send(
+            text_data=json.dumps({"type": "poll_update", "results": results})
+        )
--- polls/routing.py
+++ polls/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
+
 from . import consumers
 
 websocket_urlpatterns = [
     # This regex matches ws://.../ws/poll/POLL_ID/
-    re_path(r'ws/poll/(?P<poll_id>\w+)/$', consumers.PollConsumer.as_asgi()),
+    re_path(r"ws/poll/(?P<poll_id>\w+)/$", consumers.PollConsumer.as_asgi()),
 ]

f7851f092ebf7fda54554b26dd578ecdc1beaccf
aniebietafia <aniebietafia87@gmail.com>
2025-11-26 20:28:49 +0100

    chore: setup Dockerfile
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

--- core/celery.py
+++ core/celery.py
@@ -1,16 +1,17 @@
 import os
-
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("celery")
-
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+# Heroku-specific optimizations
+app.conf.update(
+    worker_prefetch_multiplier=1,
+    worker_max_tasks_per_child=1000,
+    task_acks_late=True,
+    task_reject_on_worker_lost=True,
+)
+
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
